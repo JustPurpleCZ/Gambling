283bfcb85930e9485e77fe5f6f38aa1dd3b6df35
Your Name <adam.zahor@icloud.com>
2025-12-06 13:47:08 +0100

    dice reposition again

--- dices-game.js
+++ dices-game.js
@@ -873,7 +873,7 @@ async function lockDie(die) {
   const containerW = gameContainer.clientWidth;
   const containerH = gameContainer.clientHeight;
   const targetX = containerW * 0.0275; 
-  const targetY = (containerH * 0.215) + (lockedDice.length * (containerH * 0.10));
+  const targetY = (containerH * 0.23) + (lockedDice.length * (containerH * 0.10));
   
   animateToPosition(die.element, targetX, targetY, () => {
     const overlay = document.createElement('div');
