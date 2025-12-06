e5494a84c6b876bbf8bbdedf06c7b5c30021371f
Your Name <adam.zahor@icloud.com>
2025-12-06 22:01:54 +0100

    collect pos change

--- dices-game.js
+++ dices-game.js
@@ -1135,8 +1135,8 @@ function collectAllDiceIntoCup() {
   const finalCupYPercent = 70;
   
   // Animate dice to an off-screen collection point first
-  const collectXPercent = 80;
-  const collectYPercent = 110;
+  const collectXPercent = 75;
+  const collectYPercent = 105;
   const collectTargetX = canvas.offsetLeft + vwToPx(collectXPercent);
   const collectTargetY = canvas.offsetTop + vhToPx(collectYPercent);
   
