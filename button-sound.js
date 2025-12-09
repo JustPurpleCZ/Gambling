8b92d97039fbf80bf1c117b70afe215bb5805dd5
Your Name <adam.zahor@icloud.com>
2025-12-09 19:20:05 +0100

    BUTTON CLICK 2

--- button-sound.js
+++ button-sound.js
@@ -1,4 +1,4 @@
-const clickSound = new Audio('sound/buttonClick.m4a');
+const clickSound = new Audio('sound/buttonClick2.m4a');
 //clickSound.volume = 0.5;
 
 document.addEventListener('click', (e) => {
